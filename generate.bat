00c6809600a78c082087dec4bf815a25d020840f
Benoit Lavenier <benoit.lavenier@e-is.pro>
2019-12-05 15:32:52 +0100

    [enh] Do not generate metadata inside SVG

--- generate.bat
+++ generate.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Converting all PlatUML files from %~dp0 into SVG...
-java -jar lib/plantuml.jar -tsvg "./**.puml" -charset UTF-8 -progress -duration
+java -jar lib/plantuml.jar -tsvg "./**.puml" -charset UTF-8 -progress -duration  -nometadata
 echo Done
