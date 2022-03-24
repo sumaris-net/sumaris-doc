bfd1fbb9b4f56303506df52fe834a4b16894135e
Benoit Lavenier <benoit.lavenier@e-is.pro>
2022-03-24 12:55:55 +0100

    [enh] Add Reveal.js slides

---
+++ gulpfile.js
@@ -0,0 +1,246 @@
+'use strict';
+
+const gulp = require('gulp'),
+  fs = require("fs"),
+  useref = require('gulp-useref'),
+  filter = require('gulp-filter'),
+  uglify = require('gulp-uglify-es').default,
+  sourcemaps = require('gulp-sourcemaps'),
+  lazypipe = require('lazypipe'),
+  gulpif = require('gulp-if'),
+  zip = require('gulp-zip'),
+  minifyCss = require('gulp-clean-css'),
+  merge = require('merge2'),
+  csso = require('gulp-csso'),
+  replace = require('gulp-replace'),
+  log = require('fancy-log'),
+  del = require('del'),
+  colors = require('ansi-colors'),
+  {argv} = require('yargs'),
+  browserSync = require('browser-sync').create(),
+  header = require('gulp-header'),
+  footer = require('gulp-footer')
+;const pkg = require("./package.json");
+
+const uglifyBaseOptions = {
+  toplevel: true,
+  warnings: true,
+  mangle: {
+    reserved: ['App']
+  },
+  compress: {
+    global_defs: {
+      "@console.log": "alert"
+    },
+    passes: 2
+  },
+  output: {
+    beautify: false,
+    preamble: "/* minified */",
+    max_line_len: 120000
+  }
+};
+
+/* --------------------------------------------------------------------------
+   -- Serve
+   --------------------------------------------------------------------------*/
+
+const paths = {
+  resources: ['src/**/*.md', 'src/images*/**/*', 'src/data*/**/*'],
+  doc_md: ['user-manual*/**/*.md', 'architecture*/**/*.md', 'model*/**/*.md', 'use-case*/**/*.md'],
+  src_html: ['src/**/*.html'],
+  src_css: ['src/css/*.css'],
+  src_js: ['src/**/*.js'],
+  src_layout: ['src/layout/*.html']
+};
+
+function watch() {
+
+  // Watch resources
+  gulp.watch(paths.resources, appCopyResources);
+
+  // Watch doc md
+  gulp.watch(paths.doc_md, appCopyDocResources);
+
+  // Watch JS + html
+  gulp.watch([...paths.src_html, ...paths.src_js, ...paths.src_css, ...paths.src_layout], appHtml);
+}
+
+
+function serve(cb) {
+  // Launch browser
+  browserSync.init({
+    watch: true,
+    server: "./dist",
+    watchOptions: {
+      ignoreInitial: true
+    }
+  });
+  cb();
+}
+
+/* --------------------------------------------------------------------------
+   -- Build the web (ZIP) artifact
+   --------------------------------------------------------------------------*/
+
+function parsePackage() {
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+}
+
+
+function appClean() {
+  return del([
+    'dist/'
+  ]);
+}
+function appCopyResources() {
+
+  log(colors.green('Copy resources files...'));
+  // Copy files to dist
+  return  gulp.src([
+    'src/**/*.md',
+    'src/images*/**/*',
+    'src/data*/**/*'
+  ])
+  .pipe(gulp.dest('dist'))
+  .pipe(browserSync.stream());
+}
+
+
+function appCopyDocResources() {
+
+  log(colors.green('Copy md files...'));
+  // Copy files to dist
+  return  gulp.src(paths.doc_md)
+      .pipe(gulp.dest('dist'))
+      .pipe(browserSync.stream());
+}
+
+function appCopyExternalResources() {
+
+  log(colors.green('Copy external resources files...'));
+  return merge(
+
+    // Copy files to dist
+    gulp.src([
+      'node_modules/katex/dist/*fonts/**',
+      'node_modules/reveal.js-plugins/menu/menu.css',
+      'node_modules/reveal.js-plugins/menu/font-awesome*/**/*'
+    ])
+    .pipe(gulp.dest('dist')),
+
+    // Copy to dist/lib
+    gulp.src([
+      'node_modules/reveal.js-plugins/chalkboard/img*/*',
+    ])
+    .pipe(gulp.dest('dist/lib')),
+
+    // Copy dist/css
+    gulp.src([
+      'node_modules/reveal.js/dist/theme/fonts/source-sans-pro/**',
+      'node_modules/reveal.js/dist/theme*/*.css'
+    ])
+    .pipe(gulp.dest('dist/css'))
+  );
+}
+
+function appHtml() {
+  const enableUglify = false; // argv.release || argv.uglify || false;
+  const pkg = parsePackage();
+
+  log(colors.green('Processing HTML files...'));
+
+  const htmlPaths = [...paths.src_html, '!src/layout/*.*', '!src/about.html'];
+  const htmlFilter = filter(htmlPaths, {restore: true});
+
+  let uglifyDone = false;
+  const needUglify = (file) => {
+    const enable = !uglifyDone && enableUglify && file.path.endsWith('vendor.js');
+    if (enable) {
+      log(colors.green('Minify JS Files...'));
+      log(colors.grey('Minifying ' + colors.bold(file.path) + '... '));
+      file.path = file.path.replace('vendor.js', 'vendor.min.js');
+      uglifyDone = true;
+    }
+    return enable;
+  }
+
+  // Process index.html
+  return gulp.src(htmlPaths)
+
+    .pipe(header(fs.readFileSync('src/layout/header.html', 'utf8'), {title: pkg.description}))
+    .pipe(footer(fs.readFileSync('src/layout/footer.html', 'utf8'), {version: pkg.version}))
+
+    // Concatenate JS and CSS files (using gulp-useref)
+    .pipe(useref())
+    .pipe(useref({}, lazypipe().pipe(sourcemaps.init, { loadMaps: true })))
+
+    // Process JS
+    .pipe(gulpif(needUglify, uglify(uglifyBaseOptions))) // Minify javascript files
+
+    // Process CSS
+    .pipe(gulpif('*.css', csso())) // Minify any CSS sources
+
+    // Add version to file path
+    .pipe(htmlFilter)
+    //.pipe(replace(/"(lib\/vendor)\.js"/g, '"$1.min.js"'))
+    .pipe(replace(/"(lib\/[a-zA-Z0-9.]+)\.js"/g, '"$1.js?v=' + pkg.version + '"'))
+    .pipe(replace(/"(css\/default)\.css"/g, '"$1.css" id="theme"'))
+    .pipe(replace(/"(css\/[a-zA-Z0-9]+)\.css"/g, '"$1.css?v=' + pkg.version + '"'))
+    .pipe(htmlFilter.restore)
+
+    .pipe(sourcemaps.write('maps'))
+
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+}
+
+
+function appZip() {
+  const packageObj = parsePackage();
+  const projectName = packageObj.name;
+  const version = packageObj.version;
+
+  return gulp.src(['dist/**/*.*', '!dist/*.zip'])
+    .pipe(zip(projectName + '-v'+version+'.zip'))
+    .pipe(gulp.dest('dist'));
+}
+
+function buildSuccess(done) {
+  const packageObj = parsePackage();
+  const projectName = packageObj.name;
+  const version = packageObj.version;
+  log(colors.green("Archive created at: 'dist/"+ projectName +"-v" + version + ".zip'"));
+  if (done) done();
+}
+
+function help() {
+  log(colors.green("Usage: gulp {clean|build} OPTIONS"));
+  log(colors.green(""));
+  log(colors.green("NAME"));
+  log(colors.green(""));
+  log(colors.green("  clean                       Clean build directory"));
+  log(colors.green("  build                       Build from sources (HTML, CSS and JS)"));
+  log(colors.green(""));
+  log(colors.green("OPTIONS"));
+  log(colors.green(""));
+  log(colors.green("  --release                   Release build (with uglify and sourcemaps)"));
+  log(colors.green("  --uglify                    Build using uglify plugin"));
+}
+
+/* --------------------------------------------------------------------------
+   -- Define public tasks
+   --------------------------------------------------------------------------*/
+
+const prepare = gulp.parallel(appCopyResources, appCopyDocResources, appCopyExternalResources);
+const compile = gulp.series(prepare, appHtml);
+const build = gulp.series(appClean, compile, appZip, buildSuccess);
+
+exports.help = help;
+exports.clean = appClean;
+exports.build = build;
+exports.compile = compile;
+exports.serve =  gulp.series(compile, serve, watch)
+
+exports.default = compile;
+
---
+++ src/js/app.js
@@ -0,0 +1,244 @@
+(function( root, factory ) {
+  if( typeof exports === 'object' ) {
+    // Node. Does not work with strict CommonJS.
+    module.exports = factory();
+  } else {
+    // Browser globals.
+    root.App = factory();
+  }
+}( this, function() {
+
+  'use strict';
+
+  var App;
+  var menu, notes;
+
+  function initialize(opts) {
+    chainByCallback([
+      $(document).ready,
+      loadExternalFiles,
+      (done) => initReveal(opts, done),
+      onRevealReady,
+      () => console.info('[app] App is ready!')
+    ]);
+  }
+
+  function chainByCallback(jobs) {
+    let result;
+    for (let i=jobs.length-1; i>=0; i--) {
+      const job = jobs[i];
+      const callback = result || undefined;
+      if (callback) {
+        result = () => job(callback);
+      }
+      else {
+        result = job
+      }
+    }
+    return result();
+  }
+
+  function loadExternalFiles(callback) {
+    console.debug('[app] Loading external sources files');
+    var sections = $("section[data-src$='.html']");
+    if (sections.length === 0) {
+      callback();
+      return;
+    }
+
+    var loadedSections = 0;
+    var checkIfLoaded = () => {
+      loadedSections++;
+      if (loadedSections === sections.length) {
+        callback();
+      }
+    }
+    sections.each((i, section) => {
+      var source = section.getAttribute('data-src');
+      $(section).load(source, () => checkIfLoaded());
+    });
+  }
+
+
+  function initReveal(opts, callback) {
+
+    console.debug('[app] Initialize Reveal...');
+
+    // Full list of configuration options available here:
+    // https://github.com/hakimel/reveal.js#configuration
+    Reveal.initialize({
+      controls: true,
+      progress: true,
+      history: true,
+      center: true,
+      mouseWheel: true,
+      slideNumber: true,
+      keyboard: true,
+      fragments: true,
+      controlsBackArrows: 'faded',
+      pdfMaxPagesPerSlide: 1,
+      hideInactiveCursor: true,
+      touch: true,
+
+      ...opts,
+
+      customcontrols: {
+        controls: [
+          {
+            icon: '<i class="fa fa-pen-square"></i>',
+            title: 'Toggle chalkboard (B)',
+            action: 'RevealChalkboard.toggleChalkboard();'
+          },
+          {
+            icon: '<i class="fa fa-pen"></i>',
+            title: 'Toggle notes canvas (C)',
+            action: 'RevealChalkboard.toggleNotesCanvas();'
+          },
+          {
+            icon: '<i class="fa fa-eraser"></i>',
+            title: 'Clear chalkboard (Suppr)',
+            action: 'RevealChalkboard.clear();'
+          }
+        ]
+      },
+      chart: {
+        defaults: {
+          color: 'lightgray', // color of labels
+          scale: {
+            beginAtZero: true,
+            ticks: { stepSize: 1 },
+            grid: { color: "lightgray" } , // color of grid lines
+          },
+        },
+        line: { borderColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ] },
+        bar: { backgroundColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ]},
+        pie: { backgroundColor: [ ["rgba(0,0,0,.8)" , "rgba(220,20,20,.8)", "rgba(20,220,20,.8)", "rgba(220,220,20,.8)", "rgba(20,20,220,.8)"] ]},
+        radar: { borderColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ]},
+      },
+      katex: { // Formula
+        local: '',
+        delimiters: [
+          {left: '$$', right: '$$', display: true},
+          {left: '$', right: '$', display: false},
+          {left: '\\(', right: '\\)', display: false},
+          {left: '\\[', right: '\\]', display: true}
+        ],
+        ignoredTags: ['script', 'noscript', 'style', 'textarea', 'pre']
+      },
+      menu: { // Menu works best with font-awesome installed: sudo apt-get install fonts-font-awesome
+        transitions: false,
+        markers: true,
+        hideMissingTitles: true,
+        themes: false,
+        custom: [
+          { title: 'Aide', icon: '<i class="fa fa-question-circle"></i>',
+            content: '<ul class="slide-menu-items">' +
+              '<a class="slide-menu-item" style="text-decoration: none;" data-transition="none" onclick="RevealMenu.toggle();Reveal.toggleHelp();" href="#">Raccourcis clavier (?)</a>'+
+              '<a class="slide-menu-item" style="text-decoration: none;" data-transition="none" onclick="App.showNotes()" href="#">Ouvrir la vue présentateur (S)</a>'+
+              '<a class="slide-menu-item" style="text-decoration: none;" data-transition="none" onclick="App.showControls()" href="#">Afficher les contrôles</a>'+
+              '<a class="slide-menu-item" style="text-decoration: none;" data-transition="none" onclick="App.showChalkboard()" href="#">Afficher le tableau (B)</a>'+
+              '<a class="slide-menu-item" style="text-decoration: none;" data-transition="none" onclick="App.writeToSlide()" href="#">Ecrire sur les diapos (C)</a>'+
+              '<a class="slide-menu-item" style="text-decoration: none;" data-transition="none" onclick="App.clearChalkboard()" href="#">Nettoyer le tableau (Suppr)</a>'+
+              '</ul>'
+          },
+          { title: 'A propos', icon: '<i class="fa fa-info"></i>', src: 'about.html' }
+        ]
+      },
+      chalkboard: { // font-awesome.min.css must be available
+        storage: "chalkboard-monnaie-libre",
+        colorButtons: 5
+      },
+      plugins: [
+        RevealMarkdown,
+        RevealMenu,
+        RevealNotes,
+        RevealHighlight,
+        RevealChart,
+        RevealMath.KaTeX,
+        RevealChalkboard,
+        RevealCustomControls
+      ],
+    });
+
+    Reveal.on( 'ready', callback);
+  }
+
+  function onRevealReady(callback){
+    const plugins = Reveal.getPlugins();
+    menu = plugins.menu;
+    notes = plugins.notes;
+
+    // Add header/footer
+    var header = $('#header').html();
+    if (header) {
+      if (window.location.search.match( /print-pdf/gi ) ) {
+        Reveal.addEventListener( 'ready', function( event ) {
+          $('.slide-background').append(header);
+          callback();
+        });
+      }
+      else {
+        $('div.reveal').append(header);
+        callback();
+      }
+    }
+    else {
+      callback();
+    }
+  }
+
+  function showNotes() {
+    menu.toggle();
+    notes.open();
+    Reveal.configure({
+      controls:false,
+      menu: {
+        openButton: false
+      }
+    });
+    var elements = document.getElementsByClassName('slide-menu-button');
+    elements[0].classList.add('hidden');
+
+    var customcontrolsDiv = document.getElementById('customcontrols');
+    customcontrolsDiv.classList.add('hidden');
+
+  }
+
+  function showControls() {
+    menu.toggle();
+    Reveal.configure({
+      controls:true
+    });
+    var elements = document.getElementsByClassName('slide-menu-button');
+    elements[0].classList.remove('hidden');
+
+    var customcontrolsDiv = document.getElementById('customcontrols');
+    customcontrolsDiv.classList.remove('hidden');
+  }
+
+  function showChalkboard() {
+    //RevealChalkboard.resetAll();
+    RevealChalkboard.toggleChalkboard();
+    return false;
+  }
+
+  function clearChalkboard() {
+    RevealChalkboard.clear();
+  }
+
+  function writeToSlide() {
+    RevealChalkboard.toggleNotesCanvas();
+  }
+
+  App = {
+    initialize,
+    showNotes,
+    showControls,
+    showChalkboard,
+    clearChalkboard,
+    writeToSlide
+  };
+
+  return App;
+
+}));
