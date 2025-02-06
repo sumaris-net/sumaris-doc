bd3201d3cb1f5bbeaaf93494b6af5b0b15c780ec
vf1977 <vincent.fachero@ifremer.fr>
2025-02-06 17:20:34 +0100

    [mex] Update resolution detail for user connexion problem

--- projects/activity-calendar/mex/v2.9/sql/mep/RESOLVE_LOGIN_PROBLEM.sql
+++ projects/activity-calendar/mex/v2.9/sql/mep/RESOLVE_LOGIN_PROBLEM.sql
@@ -4,8 +4,12 @@
 -- Récupération de l'id de l'utilisateur qui n'arrive pas à se connecter
 select id from person where id = 2749;
 
+## Test de connexion sur sumaris-app (HARMONIE_VAL)
+-- Récupération de la PUBKEY dans les logs
+-- Feb 06 11:10:14 visi-docker04-val dockersvc_sumaris-pod[741852]: 2025-02-06 11:10:14,019 TRACE [http-nio-8080-exec-8] o.h.type.descriptor.sql.BasicBinder
+-- : binding parameter [1] as [VARCHAR] - [3T4K8UvcDPe3LWvQnq6iGR8zErzCoZAeVmUqeszyWCxz]
 
 ## Schéma SIH2_ADAGIO_DBA_SUMARIS_MAP
 
--- Insertion d'une pubkey temporaire pour l'utilisateur qui n'arrive pas à se connecter
-insert into m_person values (2749, null, 'temporary_value_more_than8_caracters', null);
+-- Insertion de la pubkey pour l'utilisateur qui n'arrive pas à se connecter
+insert into m_person values (2749, null, '3T4K8UvcDPe3LWvQnq6iGR8zErzCoZAeVmUqeszyWCxz', null);
